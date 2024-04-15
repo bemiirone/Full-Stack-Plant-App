7022336a854c84a1be2c4795bbf9b15c04ce0282
Bemi Irone <bemidev@nsmultimedia.co.uk>
2024-04-15 14:25:08 +0100

    env config

--- src/app/plants/plants.service.ts
+++ src/app/plants/plants.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Plant } from './plant.interface';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlantsService {
 
-  private apiUrl = 'http://localhost:3000/plants';
+  private apiUrl = `${environment.apiUrl}/plants`;
 
   constructor(private http: HttpClient) { }
 
